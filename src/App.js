191d5995681faf301f6d6b2ac5073240762363b9
johnyvesmarin29 <johnyvesmarin@gmail.com>
2021-05-03 12:03:22 +0800

    add contacts component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import ContactCard from "./ContactCard";
 
 function App() {
+  // const contacts = [
+  //   {
+  //     avatarUrl: "https://via.placeholder.com/150",
+  //     name: "Jenny Lane",
+  //     email: "jennylane@gmail.com",
+  //     age: 25,
+  //   },
+  //   {
+  //     avatarUrl: "https://via.placeholder.com/150",
+  //     name: "John Doe",
+  //     email: "johndoe@gmail.com",
+  //     age: 28,
+  //   },
+  //   {
+  //     avatarUrl: "https://via.placeholder.com/150",
+  //     name: "Josh Chan",
+  //     email: "joshchan@gmail.com",
+  //     age: 22,
+  //   },
+  // ];
+
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=5")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setResults(data.results)
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Random User Card</h1>
+      {results.map((result, index) => {
+        return (
+          <ContactCard
+            key={index}
+            avatarUrl={result.picture.large}
+            name={result.name.first}
+            email={result.email}
+            age={result.dob.age}
+          />
+        );
+      })}
     </div>
   );
 }
-
 export default App;
